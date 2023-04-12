42e53185679aee453565e825cefce97ea0fdf5c4
zobeck <ada_hack38@hackademy.mx>
2023-04-12 15:06:08 -0600

    PAsando props a componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
-import Hola from './components/saludo';
-import Fruteria from './components/fruteria';
+import './styles/catalogo.css';
+import Libro from './components/catalogo';
+
 function App() {
   return (
-    <div className="App">
-      <Hola name="" edad = {69} pais="" />  
-      <hr></hr>
-    <Fruteria name=" Melon" precio={10} stock="true" />
+    <div className="main">
+    <Libro title="Nieve" 
+    author="Orhan Pamuk"
+    description="En mitad de una tormenta de nieve..." 
+    price= {399} 
+    stock={true} />
+
+<Libro title="Soy un gato" 
+    author="Natsume Soseki"
+    description="«Soy un gato, aunque todavía no tengo nombre.»" 
+    price= {560} 
+    stock={false} />
+
+     <Libro title="Lo bello y lo triste" 
+    author="Yasunari Kawabata"
+    description="En mitad de una tormenta de nieve..." 
+    price= {260} 
+    stock={true} />
+
     </div>
   );
 }
--- src/components/ficha.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Ficha() {
-  return (
-    <div >Ficha</div>
-  )
-}
